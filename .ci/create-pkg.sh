8522993509b458ded979d0b2d83b874d1e9913a2
fedor <fedor.korotkov@gmail.com>
2023-02-24 14:06:41 -0500

    Embed provisioning profile in `*.pkg`
    Related to #243

--- .ci/create-pkg.sh
+++ .ci/create-pkg.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-export VERSION="${CIRRUS_TAG:-0}"
+export VERSION="${CIRRUS_TAG:-SNAPSHOT}"
 
-mkdir -p .ci/pkg/
-cp .build/arm64-apple-macosx/debug/tart .ci/pkg/
-pkgbuild --root .ci/pkg --version $VERSION --install-location /usr/local/bin/ --identifier com.github.cirruslabs.tart --sign "Developer ID Installer: Cirrus Labs, Inc. (9M2P8L4D89)" "./dist/Tart-$VERSION.pkg"
+mkdir -p .ci/template/Tart.app/Contents/MacOS/Tart
+cp .build/arm64-apple-macosx/debug/tart .ci/template/Tart.app/Contents/MacOS/Tart
+pkgbuild --root .ci/template --version $VERSION --install-location '/Library/Application Support/Tart' --identifier com.github.cirruslabs.tart --sign 'Developer ID Installer: Cirrus Labs, Inc. (9M2P8L4D89)' "./dist/Tart-$VERSION.pkg"
 xcrun notarytool submit "./dist/Tart-$VERSION.pkg" --keychain-profile "notarytool" --wait
 xcrun stapler staple "./dist/Tart-$VERSION.pkg"
---
+++ .ci/template/scripts/postinstall.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+cat >/usr/local/bin/tart <<EOL
+#!/bin/sh
+exec '/Library/Application Support/Tart/Tart.app/Contents/MacOS/Tart' "$@"
+EOL
+ 
+chmod +x /usr/local/bin/tart
