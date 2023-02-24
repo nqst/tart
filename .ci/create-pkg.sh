d95af5f483ba8ef72ee50cd440782a7c06027229
fedor <fedor.korotkov@gmail.com>
2023-02-24 14:48:41 -0500

    Fixed folder structure

--- .ci/create-pkg.sh
+++ .ci/create-pkg.sh
@@ -4,7 +4,7 @@ set -e
 
 export VERSION="${CIRRUS_TAG:-SNAPSHOT}"
 
-mkdir -p .ci/template/Tart.app/Contents/MacOS/Tart
+mkdir -p .ci/template/Tart.app/Contents/MacOS
 cp .build/arm64-apple-macosx/debug/tart .ci/template/Tart.app/Contents/MacOS/Tart
 pkgbuild --root .ci/template --version $VERSION --install-location '/Library/Application Support/Tart' --scripts .ci/template/scripts/ --identifier com.github.cirruslabs.tart --sign 'Developer ID Installer: Cirrus Labs, Inc. (9M2P8L4D89)' "./dist/Tart-$VERSION.pkg"
 xcrun notarytool submit "./dist/Tart-$VERSION.pkg" --keychain-profile "notarytool" --wait
